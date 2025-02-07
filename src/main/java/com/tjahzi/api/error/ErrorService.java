f2a5ccf0cf6982978f45cb0b6c5a3af4b778359c
ssol-park <ksttlov.naver@hanmail.net>
2025-02-07 15:15:00 +0900

    refactor: 비동기 작업을 위한 커스텀 쓰레드 풀 추가
     - 비동기 작업을 위한 커스텀 ThreadPoolTaskExecutor 생성
     - MDC 전파를 위해 TaskDecorator 적용하여 traceId 유지

--- src/main/java/com/tjahzi/api/error/ErrorService.java
+++ src/main/java/com/tjahzi/api/error/ErrorService.java
@@ -3,32 +3,34 @@ package com.tjahzi.api.error;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ErrorService {
     private static final Logger logger = LoggerFactory.getLogger(ErrorService.class);
     private static final Random RANDOM = new Random();
 
-    private final ExecutorService executorService;
-    public ErrorService(@Qualifier("mdcAwareExecutorService") ExecutorService executorService) {
-        this.executorService = executorService;
+    private final ThreadPoolTaskExecutor executor;
+
+    public ErrorService(@Qualifier("mdcAwareTaskExecutor") ThreadPoolTaskExecutor executor) {
+        this.executor = executor;
     }
 
     public void getRandomError() {
         int idx = RANDOM.nextInt(5);
         logger.info("Start Random Error .. idx: {}", idx);
 
-        executorService.submit(() -> {
+        CompletableFuture.runAsync(() -> {
             try {
                 doJob(idx);
             } catch (RuntimeException e) {
                 logger.error("{}", e.getMessage(), e);
             }
-        });
+        }, executor);
     }
 
     private void doJob(int idx) {
